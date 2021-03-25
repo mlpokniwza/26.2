c97014b1b6a4af722c30feeb1f4250dab6a37544
Phanyapat Chalermchai <kayzazz1@hotmail.com>
2021-03-25 22:09:50 +0700

    Enter

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,27 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product-item/product';
+
 @Component({
-selector: 'app-product-list',
-templateUrl: './product-list.component.html',
-styleUrls: ['./product-list.component.css']
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-products: Product[];
-constructor() {
- this.products = [];
- this.products.push({
- name: 'สามโอ',
- price: 990
- });
- this.products.push({
- name: 'แตงโม',
- price: 1000
- });
- this.products.push({
- name: 'มะพร้าวนํ้าหอม',
- price: 500
- });
-}
+  products: Product[];
+  constructor() {
+    this.products = [];
+    this.products.push({
+      name: 'สามโอ',
+      price: 990
+    });
+    this.products.push({
+      name: 'แตงโม',
+      price: 1000
+    });
+    this.products.push({
+      name: 'มะพร้าวนํ้าหอม',
+      price: 500
+    });
+  }
 
   ngOnInit() {}
- }
+}
