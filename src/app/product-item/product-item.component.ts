bad8c3e5a95f04a95560b79c31bd593edecf111b
Phanyapat Chalermchai <kayzazz1@hotmail.com>
2021-03-28 21:14:16 +0700

    Enter

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,24 +1,19 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core' ;
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Product } from '../product';
-
 @Component({
-  selector: 'app-product-item' ,
-  templateUrl: './product-item.component.html' ,
-  styleUrls: ['./product-item.component.css' ]
+selector: 'app-product-item',
+templateUrl: './product-item.component.html',
+styleUrls: ['./product-item.component.css']
 })
 export class ProductItemComponent implements OnInit {
-  @Input()
-  product: Product;
-
-  @Output()
-  OnSelected: EventEmitter<Product> = new EventEmitter();
-
-  constructor() {}
-  
-  ngOnInit() {}
-
-  select() {
-    this.OnSelected.emit(this.product);
-  }
-
+ @Input()
+product: Product;
+ @Output()
+OnSelected: EventEmitter<ProductItemComponent> = new EventEmitter();
+isSelected: boolean;
+constructor() {}
+ngOnInit() {}
+select() {
+ this.OnSelected.emit(this);
+ }
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,34 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component,OnInit, ViewChild, ViewChildren, QueryList} from '@angular/core';
 import { Product } from '../product';
-
+import { ProductItemComponent } from '../product-item/product-item.component';
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+selector: 'app-product-list',
+templateUrl: './product-list.component.html',
+styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  products: Product[];
-  constructor() {
-    this.products = [];
-    this.products.push({
-      name: 'สามโอ',
-      price: 990
-    });
-    this.products.push({
-      name: 'แตงโม',
-      price: 1000
-    });
-    this.products.push({
-      name: 'มะพร้าวนํ้าหอม',
-      price: 500
-    });
-  }
-  
-  ngOnInit() {}
-
-  selectedProduct(product: Product) {
-    alert(`Product ${product.name} selected`);
-    }
+ @ViewChildren(ProductItemComponent)
+productItems: QueryList<ProductItemComponent>;
+products: Product[];
+constructor() {
+ this.products = [];
+ }
+ngOnInit() {}
+selectedProduct(productComponent: ProductItemComponent) {
+ alert(`Product ${productComponent.product.name} selected`);
+ this.productItems.forEach(p => {
+ p.isSelected = false;
+ });
+ productComponent.isSelected = true;
+ }
 }
-   
-      
